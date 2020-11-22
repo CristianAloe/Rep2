1dafe59291086d025173d3942013f277a2464435
Cristian <four46cri@yahoo.it>
2020-11-22 15:36:10 +0100

    14.7) aggiungo il file users.component.html 14.8) Aggiungo il file users.component.css

--- UMS/src/app/users/users.component.ts
+++ UMS/src/app/users/users.component.ts
@@ -4,7 +4,9 @@ import {Component} from '@angular/core';
 
 @Component({  //14.5)per mettere questo è necessario l'import del component
     selector: 'app-users', //14.6)richiamo questo componente  in app-component.html
-    template: '<h2> Users </h2>'
+    templateUrl: './users.component.html', //14.7) aggiungo il file users.component.html
+    styleUrls: ['./users.component.css']  //14.8) Aggiungo il file users.component.css
+    
 })
 
 export class UsersComponent{  //14.2) Per Creare il componente manualmente, creo la classe UsersComponent,  14.3)e dichiaro  questa classe in app.module.ts, 14.4)di conseguenza richiamo il @Component,
