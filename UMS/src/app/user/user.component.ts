7bda4501144e2e0de7b8615ebac55b73ea1d51fb
Cristian <four46cri@yahoo.it>
2020-12-01 19:08:24 +0100

    20.4)dentro il decoratore (di user.component.ts) possiamo mettre anche l'alias user-data (che deve essere presente anche in users.component.html) 20.5) Quindi se utilizziamo l'alias "user-data" allora la variabile deve essere anche qui (in users.component.html)chiamata user-data-->

--- UMS/src/app/user/user.component.ts
+++ UMS/src/app/user/user.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit { //19.1 è stato creato questo compoennte dalla riga di comando e vediamo che ha già l'ngOnint() //19.2 possiamo testarlo andando ad inserirlo su app.component.html
-@Input() user  //20.2) Per poter utilizzare la variabile user nel componente app-user è necessario dichiararlo qui.  Ma in più dobbiamo dire che è una variabile di tipo input. ovvero che è printa a ricevere dati, mettendo il decoratore @Input() a cui all'interno posso mettere anche un alias chiamato "user-data"
+@Input('user-data') user  //20.2) Per poter utilizzare la variabile user nel componente app-user è necessario dichiararlo qui.  Ma in più dobbiamo dire che è una variabile di tipo input. ovvero che è printa a ricevere dati, mettendo il decoratore @Input() a cui all'interno posso mettere anche un alias chiamato "user-data". 20.4)dentro il decoratore possiamo mettre anche l'alias user-data (che deve essere presente anche in users.component.html)
   constructor() { }
 
   ngOnInit() {
