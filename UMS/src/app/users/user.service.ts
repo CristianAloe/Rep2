c66b3cf542cb5a25f313c4d097732e13da201ae4
Cristian <four46cri@yahoo.it>
2020-12-01 10:52:12 +0100

    //17.7 per dire ad Anular che questo servizio ha delle dipendenze  e che questo servizio verrà iniettato e dovrà risolvere tutte le dipendenze di questo servizo noi possiamo mettere un decoratore che si chiama @Injectable()

--- UMS/src/app/users/user.service.ts
+++ UMS/src/app/users/user.service.ts
@@ -1,5 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable() //17.7 per dire ad Anular che questo servizio ha delle dipendenze  e che questo servizio verrà iniettato e dovrà risolvere tutte le dipendenze di questo servizo noi possiamo mettere un decoratore che si chiama @Injectable() 
 export class UserService { //17.1 creo questa classe UserService
                            //17.3 Andrà a richiamare questa classe con la DI in users.component.ts
+    constructor(){
+            }                      
     getUsers(){
         return [ //17.2 Sposto i dati da user.component.ts a user.service.ts 
             {
