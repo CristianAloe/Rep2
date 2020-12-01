dc8697283f23cd2452a1f3e3791d24fac2d78c04
Cristian <four46cri@yahoo.it>
2020-12-01 19:02:10 +0100

    20.1)Sposto  i dati che voglio che compaiano sul componente figlio app-user in particolare in user.component.html 20.2) Per poter utilizzare la variabile user nel componente app-user è necessario dichiararlo qui(in user.component.ts).  Ma in più dobbiamo dire che è una variabile di tipo input. ovvero che è printa a ricevere dati, mettendo il decoratore @Input() 20.3) qui (in users.component.html) stiamo passando la variabile user  al componente figlio app-user e il suo valore sarà dato dall'oggetto "user" (vedi quello tra virgolette in rosso). Ma questa variabile user, per poter essere usata nel componente app-user, deve essere dichiarata in users.component.ts

--- UMS/src/app/user/user.component.ts
+++ UMS/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit { //19.1 è stato creato questo compoennte dalla riga di comando e vediamo che ha già l'ngOnint() //19.2 possiamo testarlo andando ad inserirlo su app.component.html
-
+@Input() user  //20.2) Per poter utilizzare la variabile user nel componente app-user è necessario dichiararlo qui.  Ma in più dobbiamo dire che è una variabile di tipo input. ovvero che è printa a ricevere dati, mettendo il decoratore @Input() a cui all'interno posso mettere anche un alias chiamato "user-data"
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
+
+
