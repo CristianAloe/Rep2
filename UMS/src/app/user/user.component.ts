16f02f3e9cbb2338efd902408b74a7bb7ee304f8
Cristian <four46cri@yahoo.it>
2020-12-02 12:23:39 +0100

    21.1) Dobbiamo cancellare gli utenti cancellandoli dall’array . Però vogliamo farlo direttamente con il componente user. Però devo iniettare a questo componente user  il servizio service. Quindi metto la dipendenza nel costruttore in questo modo 21.2) Ma ora non abbiamo ancora nessun metodo per eliminare. Quindi torno  su user.service,ts e svuoto  getUsers e  i dati li copio in una variabile chiamata users //22.3 Nel getUsers() metto return this.users così ho la possibilità di ritornare gli utenti //22.4 Mentre aggiungo un nuovo metodo deleteUser(user) ovveor che riceve un utente e poi ci cancella con il metodo splice. Il metodo splice necesita della posizione dell’utente che vogliamo eliminare e quanti elementi vogliamo eliminare (in questo caso solo 1) . Quindi per trovare l’indice  facciamo let index = this.users.indexOf(user); In questo caso diciamo che vogliamo eliminare solo un utente a partire da quella posizione

--- UMS/src/app/user/user.component.ts
+++ UMS/src/app/user/user.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../users/user.service';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -7,12 +8,13 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit { //19.1 è stato creato questo compoennte dalla riga di comando e vediamo che ha già l'ngOnint() //19.2 possiamo testarlo andando ad inserirlo su app.component.html
 @Input('user-data') user  //20.2) Per poter utilizzare la variabile user nel componente app-user è necessario dichiararlo qui.  Ma in più dobbiamo dire che è una variabile di tipo input. ovvero che è printa a ricevere dati, mettendo il decoratore @Input() a cui all'interno posso mettere anche un alias chiamato "user-data". 20.4)dentro il decoratore possiamo mettre anche l'alias user-data (che deve essere presente anche in users.component.html)
-  constructor() { }
+ 
+constructor(private userService: UserService) { } //22.1 Dobbiamo cancellare gli utenti cancellandoli dall’array . Però vogliamo farlo direttamente con il componente user. Però devo iniettare a questo componente user  il servizio service. Quindi metto la dipendenza nel costruttore in questo modo
 
   ngOnInit() {
   }
   deleteUser(){ //21.4) metto questo evento deleteUser() in user.component.ts
-    alert(this.user.lastname)
+    this.userService.deleteUser(this.user);
   }
 }
 
--- UMS/src/app/users/user.service.ts
+++ UMS/src/app/users/user.service.ts
@@ -3,39 +3,51 @@ import { Injectable } from '@angular/core';
 @Injectable() //17.7 per dire ad Anular che questo servizio ha delle dipendenze  e che questo servizio verrà iniettato e dovrà risolvere tutte le dipendenze di questo servizo noi possiamo mettere un decoratore che si chiama @Injectable() 
 export class UserService { //17.1 creo questa classe UserService
                            //17.3 Andrà a richiamare questa classe con la DI in users.component.ts
-    constructor(){
-            }                      
-    getUsers(){
-        return [ //17.2 Sposto i dati da user.component.ts a user.service.ts 
-            {
-                name : 'Cristian1',
-                lastname: 'Gio1',
-                email: 'cris@gmail.com',
-                fiscalcode: 'ASDFGHJKLQ4345',
-                province: 'Torino',
-                phone: '432432532452',
-                age: 43
-    
-            },
-            {
-                name : 'Cristian2',
-                lastname: 'Gio2',
-                email: 'cris@gmail.com',
-                fiscalcode: 'ASDFGHJKLQ4345',
-                province: 'Torino',
-                phone: '432432532452',
-                age: 43
-            },
-            {
-                name : 'Cristian3',
-                lastname: 'Gio3',
-                email: 'cris@gmail.com',
-                fiscalcode: 'ASDFGHJKLQ4345',
-                province: 'Torino',
-                phone: '432432532452',
-                age: 43
-            }
 
-        ]
+    users = [ //21.2) Ma ora non abbiamo ancora nessun metodo per eliminare. Quindi torno  su user.service,ts e svuoto  getUsers e  i dati li copio in una variabile chiamata users 
+        {
+            name : 'Cristian1',
+            lastname: 'Gio1',
+            email: 'cris@gmail.com',
+            fiscalcode: 'ASDFGHJKLQ4345',
+            province: 'Torino',
+            phone: '432432532452',
+            age: 43
+
+        },
+        {
+            name : 'Cristian2',
+            lastname: 'Gio2',
+            email: 'cris@gmail.com',
+            fiscalcode: 'ASDFGHJKLQ4345',
+            province: 'Torino',
+            phone: '432432532452',
+            age: 43
+        },
+        {
+            name : 'Cristian3',
+            lastname: 'Gio3',
+            email: 'cris@gmail.com',
+            fiscalcode: 'ASDFGHJKLQ4345',
+            province: 'Torino',
+            phone: '432432532452',
+            age: 43
+        }
+
+    ]
+
+    constructor(){
+            }   
+
+    getUsers(){ //22.3 Nel getUsers() metto return this.users così ho la possibilità di ritornare gli utenti
+        return this.users;
+    }
+
+    deleteUser(user){ ////22.4 Mentre aggiungo un nuovo metodo deleteUser(user) ovveor che riceve un utente e poi ci cancella con il metodo splice. Il metodo splice necesita della posizione dell’utente che vogliamo eliminare e quanti elementi vogliamo eliminare (in questo caso solo 1) . Quindi per trovare l’indice  facciamo let index = this.users.indexOf(user); In questo caso diciamo che vogliamo eliminare solo un utente a partire da quella posizione
+        let index = this.users.indexOf(user);
+        if(index >=0){
+            this.users.splice(index, 1)
+        }
+        
     }
 }
