ff01effd4f72f835c57ae7706c1668ac2459a72c
Cristian <four46cri@yahoo.it>
2020-12-06 12:17:17 +0100

    //25.1 Vogliamo mettere il pulsante modifica  e vogliamo  vedere che cliccando il pulsante modifica possiamo passare questo evento di modifica al parent che sarebbe il componente userss,  e da l’ users può passare questo evento //anche al componente app perché  abbiamo visto che lo userdetails si troverà direttamente nell ‘app.component.html. E vediamo che abbiamo la app-user-detail e la app-users. //25.2 Quindi dobbiamo richiamare questo . userSelected è l’utente che viene selezionato e poi mostrare se questo utente è stato selezionato //25.3 Quindi andiamo su user.component.html e facciamo il pulsante modifica UPDATE (vediamo che dentro non è necessario passargli nessun utente) al  quale al click scatena il metodo updateUser() //25.4 Poi andiamo ad aggiungere nel user.component.css updareUser //25.6 Quindi vado a mettere il metodo  updateUser() nel user.component.ts. Questo metodo prevede di prendere il valore/informazione (in questo caso un oggetto di tipo user) presente dentro la parentesi tonda di emit e lo inserisce dentro l’evento onSelectUser //25.7 Poi andiamo dal compomente padre ovvero in users.component.html dove richiamiamo l’evento onSelectUser (in celestino) il quale passa la sua informazione  (in questo caso l’oggetto user)al metodo onSelectUser($event). E questo è stato possibile poiché all’interno della parentesi abbiamo messo $event //25.8 Poi andiamo a mettere il metodo onSelectUser (in rosso) in users.component.ts //25.9 quindi noi possiamo allo stesso modo qui in users.component.ts possiamo anche mettere un evento qua in uscita, ovvero possiamo mettere in output ,un evento che lo chiamiamo o updateUser allo stesso modo tipo New event EventMeter.  addirittura che come parametro in ingresso <User>. Posso richiamare l’evento updateUser e richiamo il metodo emit e li passiamo dentro la parentesi l’user. //25.10 A questo punto questo evento updateUser viene rilanciato all'esterno è l'app component può ascoltare questo evento e rispondere a questo evento.  quindi noi andiamo ad ascoltare Questo vento updateUser qui nell’app Component.  Questo evento viene emesso da app-users e quindi noi dobbiamo ascoltarlo qui. In più quando scatenerà questo evento viene chiamato il metodo che si chiama updateUser al cui all’interno ha $event ovvero l’evento generale (in questo caso  viene passato l'utente) //25.10 A questo punto questo evento updateUser viene rilanciato all'esterno è l'app component può ascoltare questo evento e rispondere a questo evento.  quindi noi andiamo ad ascoltare Questo vento updateUser qui nell’app Component.  Questo evento viene emesso da app-users e quindi noi dobbiamo ascoltarlo qui. In più quando scatenerà questo evento viene chiamato il metodo che si chiama updateUser al cui all’interno ha $event ovvero l’evento generale (in questo caso  viene passato l'utente) //25.11 quindi qui nell’app component dobbiamo dichiarare questa variabile userSelected (che sarà l'utente selezionato) che verrà passato in questa variabiel variabiel event e in automatico verrà mostrato questo template quindi apriamo appunto app.component.ts, e metto in il metodo updateUser e mettiamo che l'utente selezionato (metto la variabile userSelected: user )Quindi questa variabile qua sarà l'utente //25.12 Poi modifichiamo il css

--- UMS/src/app/users/users.component.ts
+++ UMS/src/app/users/users.component.ts
@@ -1,7 +1,7 @@
 import { UserService } from '../services/user.service';
 import { Template } from '@angular/compiler/src/render3/r3_ast';
 
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { User } from '../interfaces/user';
 
 @Component({  //14.5)per mettere questo è necessario l'import del component
@@ -14,6 +14,8 @@ import { User } from '../interfaces/user';
 export class UsersComponent implements OnInit{  //14.2) Per Creare il componente manualmente, creo la classe UsersComponent,  14.3)e dichiaro  questa classe in app.module.ts, 14.4)di conseguenza richiamo il @Component,
     title = 'Users'; //15.1 Valore in input che deve essere inserito users.component.html con la forma {{title}}
     users: User[] = [];//15.2)Adesso proviamo ad inserire un array di dati che faremo ciclare in users.component.html
+   @Output() updateUser = new EventEmitter<User>();
+   
     constructor(private service: UserService){ //17.4 Dipendency Injection: Angular va ad instanziare automaticamente la classe UserService dal costruttore di users.component.ts
             
         }
@@ -25,5 +27,10 @@ export class UsersComponent implements OnInit{  //14.2) Per Creare il componente
     onDeleteUser(user: User){ //22.8)Quindi ora andiamo a mettere l’evento in onDeleteUser nella classe Users, Quindi veniamo qui e creiamo questo metodo onDeleteUser che riceve un utente e andiamo subito  a provare a mettere un alert e vediamo se funziona (mettendo dentro  last name)
     //alert(user.lastname)
       this.service.deleteUser(user) ////22.9 Quindi a questo punto noi passiamo chiamare servizio perché il servizio ce l'abbiamo già qui si chiama Service quindi Punto Service Delete user e ripassiamo user 
+    }
+
+    onSelectUser(user: User){
+        
+        this.updateUser.emit(user)
     }
 }
