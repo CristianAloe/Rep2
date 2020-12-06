eb564774d846e146f9d2b6abffc13a0025be1a71
Cristian <four46cri@yahoo.it>
2020-12-06 17:17:01 +0100

    //26.1 Ciao ragazzi nella precedente lezione eravamo rimasti che cliccando so dei mostrava form e adesso l’app component deve passare l'oggetto user a questo form dettail. però il form dettail  per poter ricevere quella variabile deve dichiarare quella variabile come input. qualunque variabile noi vogliamo iniettare in un componente dobbiamo dichiarare quella variabile come input. quindi Veniamo qua al componente user detail.component  e dichiariamo una variabile tipo input la e la chiamiamo user quindi di tipo  user  e guardate già che è stato importato sia io che la Direttiva c'è il decorator input //26.2 a questo punto possiamo ricevere la variabile come input e come possiamo ricevere questa variabile? Andiamo qui nell’ app.component.html e passiamo questo usersSelected che è quello che ci passa il componente users nell'evento.  andiamo dire che la variabile user  deve essere in ingresso ( Guardate che quando ci sono le parentesi quadre vuol dire che in ingresso) //26.3 Adesso vogliamo fare i l binding con unadesso dobbiamo fare con una direttiva che si chiama ng Model. E come lo possiamo fare?proviamo qua e mettiamo in ng Model e possiamo dire con chi mappare questa direttiva //26.4 Dobbiamo però importare  in app.module.ts FormsModule //26.5 Che cos'è questa che ho scritto qua dentro? Io sono di tel HTML quando noi vogliamo collegare una proprietà del form ad un oggetto e allo stesso tempo questa proprietà sia in output e input ( Guardate che le parentesi quadrate  vuol dire che è la variabile è In input e le parentesi tonde sono in output) . Quindi modificheremo il valore di input ma anche di output. //26.6 Adesso andiamo a mettere il two way per gli altri campi

--- UMS/src/app/app.module.ts
+++ UMS/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
     UserDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [UserService],//17.5)Ma questa dipendenza non sa da dove iniettarla e quindi dobbiamo andare su app.module.ts  e dobbiamo metterlo sulla voce “providers” per dirgli da dove deve essere iniettato il servizio.
   bootstrap: [AppComponent]
